80278ed105d8add450d401003388382526ebc089
Carlos Quinonez <caquinon@espol.edu.ec>
2022-08-27 17:20:42 -0500

    Creacion de clase persona

--- src/main/java/modelo/Persona.java
+++ src/main/java/modelo/Persona.java
@@ -9,5 +9,49 @@ package modelo;
  * @author LENOVO
  */
 public class Persona {
+    private String cedula;
+    private String nombre;
+    private String telefono;
+    private String email;
     
-}
+    public String getCedula(){
+        return cedula;
+    }
+    
+    public void setCedula(String dni){
+        this.cedula = dni;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String name){
+        this.nombre = name;
+    }
+    
+    public String getTelefono(){
+        return telefono;
+    }
+    
+    public void setTelefono(String tel){
+        this.telefono = tel;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    //constructor Persona
+    public Persona(String cedula, String nombre, String telefono, String email){
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;    
+    }
+}//end Persona
+
