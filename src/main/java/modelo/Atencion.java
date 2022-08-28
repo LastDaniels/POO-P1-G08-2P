b780ee4c31b858260b5a7aa854aa298405f06f57
EdisonYagual <epyagual@espol.edu.ec>
2022-08-28 01:59:14 -0500

    MODIFICACIÓN DE LAS CLASES CITA ATENCION

--- src/main/java/modelo/Atencion.java
+++ src/main/java/modelo/Atencion.java
@@ -4,10 +4,107 @@
  */
 package modelo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author LENOVO
  */
 public class Atencion {
+    private Cliente cliente;
+    private Empleado empleado;
+    private int duracionAtencion; //duracion real de la atencion
+    private Cita cita;
+    
+
+    public Atencion(Cliente cliente, Empleado empleado, int duracionAtencion, Cita cita) {
+        this.cliente = cliente;
+        this.empleado = empleado;
+        this.duracionAtencion = duracionAtencion;
+        this.cita = cita; 
+    }
+    // Consulta de una atencion por medio de la cedula
+    public static void consultarAtencion(ArrayList<Atencion> atenciones){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la consulta. "
+                + "Puede consultar por cedula del empleado, cedula del cliente, o fecha de la cita[dd/mm/yy].");
+        String consulta = sc.nextLine();
+
+        for(int i=0; i<atenciones.size();i++){
+            if((consulta.equals(atenciones.get(i).getCliente().getCedula())) || (consulta.equals(atenciones.get(i).getEmpleado().getCedula()))||
+                    (consulta.equals(atenciones.get(i).getCita().getFecha()))){
+                System.out.println("Cliente: " + atenciones.get(i).getCliente().getNombre()+", Empleado: "+atenciones.get(i).getEmpleado().getNombre()
+                +", Duracion: "+atenciones.get(i).getDuracionAtencion()+ ", Fecha: "+atenciones.get(i).getCita().getFecha()+", Hora: "+atenciones.get(i).getCita().getTime()
+                + ", Servicio: "+ atenciones.get(i).getCita().getServicio());//.getNombre()
+                //########################################33
+                         }
+        }
+
+
+        
+    }
+    
+    public static void registrarAtencion(ArrayList<Atencion> atenciones , ArrayList<Cita> citas, ArrayList<Empleado> empleados){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese numero de cedula del cliente: ");
+        String cedula = sc.nextLine();
+        //Se busca la cedula dentro de las citas
+        for(int i =0; i< citas.size(); i++){
+            if(cedula.equals(citas.get(i).getCliente().getCedula())){
+                System.out.println("Ingrese la cedula del empleado que realizo la atencion: ");
+                String empleado = sc.nextLine();
+                for(int j = 0; j<empleados.size(); j++){
+                    if(empleado.equals(empleados.get(j).getCedula())){
+                        System.out.println("Ingrese la duracion de la atencion en minutos: ");
+                        int duracion = sc.nextInt();
+                        sc.nextLine();
+                        atenciones.add(new Atencion(citas.get(i).getCliente(),empleados.get(j), duracion, citas.get(i)));
+                    }else if (i == empleados.size() - 1) {
+                        System.out.println("No se encontro empleado con esa cedula.");
+                    }
+                }
+            } else if (i == citas.size() - 1){
+                System.out.println("No se encontro cita para ese cliente.");
+            } 
+        }
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    public int getDuracionAtencion() {
+        return duracionAtencion;
+    }
+
+    public void setDuracionAtencion(int duracionAtencion) {
+        this.duracionAtencion = duracionAtencion;
+    }
+
+    public Cita getCita() {
+        return cita;
+    }
+
+    public void setCita(Cita cita) {
+        this.cita = cita;
+    }
+    
+    @Override
+    public String toString(){
+        return "";
+    }   
     
 }
--- src/main/java/modelo/Cita.java
+++ src/main/java/modelo/Cita.java
@@ -4,10 +4,141 @@
  */
 package modelo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author LENOVO
  */
 public class Cita {
+    private Cliente cliente;
+    private String fecha; //formato dd/mm/aa
+    private String time; //formato hh:mm 
+    private Empleado empleado;
+    private Servicio servicio;
+
+    public Cita(Cliente cliente, String fecha, String time, Empleado empleado, Servicio servicio) {
+        this.cliente = cliente;
+        this.fecha = fecha;
+        this.time = time;
+        this.empleado = empleado;
+        this.servicio = servicio;
+    }
+   
+    
+    //metodo crear cita //Date fecha, LocalTime time, 
+    public static void crearCita(ArrayList <Cita> citas, ArrayList<Cliente> clientes, ArrayList<Empleado> empleados, ArrayList<Servicio> servicios){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("***Crear Cita***");
+        
+        System.out.println("Ingrese cedula del cliente: ");
+        String cedula1 = entrada.nextLine();
+        int i;
+        for (i = 0; i<clientes.size(); ++i){
+            if (cedula1.equals(clientes.get(i).getCedula())){
+                System.out.println("Ingrese el servicio: ");
+                String asistencia = entrada.nextLine();
+                for (int a = 0; a<servicios.size(); a++){
+                    if (asistencia.equals(servicios.get(a))){//.getNombre()
+                        //########################3
+                        int x=0;
+                        while (x<citas.size()){
+                            System.out.println("Ingrese fecha cita con formato dd/mm/aa: ");
+                            String fecha = entrada.nextLine();
+                   
+                            System.out.println("Ingrese hora cita con formato hh:mm: ");
+                            String time = entrada.nextLine();
+                            for(int j =0; j < citas.size();j++){
+                                for(int e =0; e < citas.size(); e++){
+                                    if(fecha.equals(citas.get(j).getFecha())&& time.equals(citas.get(e).getTime())){
+                                        System.out.println("Horario no diaponible");
+                                    }
+                                    else {
+//                                        citas.add(new Cita(clientes.get(i), fecha, time, servicios.get(a).getEmpleado(), servicios.get(a)));
+                                        //citas.add(new Cita(clientes.get(i), fecha, time, servicios.get(a).getEmpleado(), servicios.get(a)));
+                                        //########################################3333
+                                        x = citas.size();
+                                        e = citas.size();
+                                        j = citas.size();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //eliminar cita
+    public static void eliminarCita(ArrayList <Cita> citas){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese cedula del cliente: ");
+        String cedula2 = entrada.nextLine();
+        //String fechaActual = "15/7/2022";
+        ArrayList<String> fechas = new ArrayList<>();
+        System.out.println("Fechas de citas pedientes: ");
+        for (int i = 0; i < citas.size(); ++i) {
+            if (cedula2.equals(citas.get(i).getCliente().getCedula())) {
+                System.out.println(citas.get(i).getFecha());
+                fechas.add(citas.get(i).getFecha());
+                //if (citas.getFecha()<fechaActual){ 
+            }
+        }
+        if (fechas.isEmpty()) {
+            System.out.println("No hay mas citas pendientes");
+        } else {
+            System.out.println("Escriba la fecha de la cita que desea eliminar [dd/mm/yy].");
+            String fecha = entrada.nextLine();
+            for (int i = 0; i < citas.size(); i++) {
+                if (fecha.equals(citas.get(i).getFecha()) && cedula2.equals(citas.get(i).getCliente().getCedula())) {
+                    citas.remove(i);
+                }
+            }
+
+        }
+    }
+
+           
+    public static void consultarCita(ArrayList <Cita> citas){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese fecha de la cita con formato dd/mm/aa: ");
+        String fecha = entrada.nextLine();
+        for (int e = 0; e<citas.size(); ++e){
+            if (fecha.equals(citas.get(e).getFecha())){
+                System.out.println("Cliente: "+ citas.get(e).getCliente().getNombre()+", Fecha: "+citas.get(e).getFecha()
+                        +", Hora: "+citas.get(e).getTime()+", Servicio: "+citas.get(e).getServicio());//.getNombre()
+                //###########################################################
+            }else if (e == citas.size() - 1){
+                System.out.println("No hay mas citas agendadas");
+            }
+        }
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public Servicio getServicio() {
+        return servicio;
+    }
+    
+    public static ArrayList<Cita> ListaCita(){
+        ArrayList<Cita> listaCitas = new ArrayList<Cita>();
+        
+        return null;
+    }
     
 }
