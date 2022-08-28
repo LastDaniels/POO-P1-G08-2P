70538395a87115ee54e952be8fc0c58b8519f7b0
Carlos Quinonez <caquinon@espol.edu.ec>
2022-08-27 22:42:01 -0500

    Modificacion de clases y creacion de txt, controladores

--- src/main/java/modelo/Cliente.java
+++ src/main/java/modelo/Cliente.java
@@ -8,27 +8,53 @@ package modelo;
  *
  * @author LENOVO
  */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
-public class Cliente extends Persona{
-    private ArrayList<Cliente> clientes;
-    private ArrayList<Representante> representantes;
-    private Representante representante;
+public class Cliente extends Persona implements Comparable<Cliente>{
+    private String nombreRepresentante;
     
-    public Representante getRepresentante(){
-        return representante;
+    public String getNombreRepresentante(){
+        return nombreRepresentante;
     }
     
-    public void setRepresentante(Representante representante){
-        this.representante = representante;
+    public void setNombreRepresentante(String nombreRepresentante){
+        this.nombreRepresentante = nombreRepresentante;
     }
     
     
     //Constructor Cliente
-    public Cliente(String cedula, String nombre, String telefono, String email, Representante representante){
+    public Cliente(String cedula, String nombre, String telefono, String email, String nombreRepresentante){
         super(cedula, nombre, telefono, email);
-        this.representante = representante;
+        this.nombreRepresentante = nombreRepresentante;
+    }
+
+    @Override
+    public int compareTo(Cliente cl) {
+        return getNombre().compareTo(cl.getNombre());
     }
     
+    public static ArrayList<Cliente> cargarClientes(String ruta){
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        
+        try(BufferedReader br = new BufferedReader(new FileReader(ruta))){
+            String sCurrentLine;
+            
+            br.readLine();
+            while((sCurrentLine = br.readLine())!=null){
+                String[] datos = sCurrentLine.split(", ");
+                Cliente cl = new Cliente(datos[0], datos[1], datos[2], datos[3], datos[4]);
+                clientes.add(cl);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Archivo no existe");
+        } catch (IOException   ex) {
+            System.out.println("Error io:"+ex.getMessage());
+        } 
+        return clientes;
+    }
 }
--- src/main/java/modelo/Representante.java
+++ src/main/java/modelo/Representante.java
@@ -4,10 +4,45 @@
  */
 package modelo;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author LENOVO
  */
-public class Representante {
+public class Representante extends Persona implements Comparable<Representante>{
     
+     //constructor Representante
+    public Representante(String cedula, String nombre, String telefono, String email){
+        super(cedula, nombre, telefono, email);
+    }
+    
+    @Override
+    public int compareTo(Representante r) {
+        return getNombre().compareTo(r.getNombre());
+    }
+    
+    public static ArrayList<Representante> cargarRepresentantes(String ruta){
+        ArrayList<Representante> representantes = new ArrayList<>();
+        
+        try(BufferedReader br = new BufferedReader(new FileReader(ruta))){
+            String sCurrentLine;
+            
+            br.readLine();
+            while((sCurrentLine = br.readLine())!=null){
+                String[] datos = sCurrentLine.split(", ");
+                Representante r = new Representante(datos[0], datos[1], datos[2], datos[3]);
+                representantes.add(r);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Archivo no existe");
+        } catch (IOException   ex) {
+            System.out.println("Error io:"+ex.getMessage());
+        } 
+        return representantes;
+    }
 }
